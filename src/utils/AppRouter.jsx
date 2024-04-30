1f9ec28ff9753771aeddfe3528d3a008531cdff7
Yuji_Itadorii <rawat141abhay@gmail.com>
2024-04-30 08:07:44 +0530

    Creating a dashboard page

--- src/utils/AppRouter.jsx
+++ src/utils/AppRouter.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
+import Dashboard from '../pages/Dashboard.jsx'
+import Navbar from '../components/Navbar.jsx'
+
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home />} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   )
 }
