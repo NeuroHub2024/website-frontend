a992deff64fcb3c3c7f589a0dd66fae6d3842d77
Vaibhav <vaibhav.bajpai1221@gmail.com>
2024-05-02 17:17:52 +0530

    Added toggle switch for theme

---
+++ src/components/ThemeToggleBtn.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import { useTheme } from '../contexts/ThemeContext'
+import styles from '../styles/themeToggleBtn.module.css'
+
+const ThemeToggleBtn = () => {
+    const {theme, toggleTheme} = useTheme()
+
+  return (
+    <label className={styles.switch}>
+        &nbsp;
+        <input checked={theme==='dark'} onClick={toggleTheme} type="checkbox" name="" id="checkbox" />
+        <span className={theme==='dark' ? styles.sliderDark : styles.sliderLight}>
+            <span className={theme==='dark' ? styles.innerSliderDark : styles.innerSliderLight}></span>
+        </span>
+    </label>
+  )
+}
+
+export default ThemeToggleBtn
