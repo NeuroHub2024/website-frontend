ee4d7618f977ef2f621dbc13d8e116c29551b403
abhay rawat <rawat141abhay@gmail.com>
2024-05-17 09:10:50 +0530

    Implementing ML Model

---
+++ src/components/UploadAssignmentComponent.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import "../styles/UploadAssignmentComponent.css";
+import { UploadOutlined } from '@ant-design/icons';
+import pdfToText from 'react-pdftotext'
+
+const UploadAssignmentComponent = () => {
+    const [file, setFile] = useState(null);
+    const [label, setLabel] = useState("");
+    const [humanPer, setHumanPer] = useState(0);
+    const [AIPer, setAIPer] = useState(0);
+    const [hidden, setHidden] = useState(true);
+
+    const handleFileChange = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+    };
+
+    const handleOnClick = async () => {
+        if (file) {
+            console.log(`Selected file: ${file.name}`);
+            const content = await pdfToText(file);
+
+            const data = {
+                text: content
+            }
+            const url = "http://13.233.253.145:3000/predict"
+
+            const response = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: { "Content-Type": "application/json" },
+            });
+
+            if (!response.ok) {
+                throw new Error(`API request failed with status ${response.status}`);
+            }
+
+            const responseData = await response.json();
+
+            if (responseData) {
+                setLabel(responseData.label === "1" ? "AI-generated" : "Human-generated");
+                setHumanPer(parseFloat(responseData["human-genrated"]).toFixed(2));
+                setAIPer(parseFloat(responseData["ai-generated"]).toFixed(2));
+            }
+
+            setHidden(false);
+
+
+        } else {
+            console.log('No file selected.');
+        }
+    };
+
+    return (
+        <>
+            <div className="upload-assignment-container">
+                <input type="file" onChange={handleFileChange} />
+                <button onClick={handleOnClick}>Check plagiarism</button>
+
+                <div className="plagrism-result-container" style={{
+                    display: hidden ? "none" : "block"
+                }}>
+                    <p>Plagrism Result: {label}</p>
+                    <p>Human Generated Percentage : {humanPer}</p>
+                    <p>AI Generated Percentage : {AIPer}</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UploadAssignmentComponent;
--- src/utils/AppRouter.jsx
+++ src/utils/AppRouter.jsx
@@ -1,21 +1,45 @@
 
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from '../pages/Home'
-import Dashboard from '../pages/Dashboard.jsx'
 import Navbar from '../components/Navbar.jsx'
 import Login from '../pages/Login.jsx'
 import Signup from '../pages/Signup.jsx'
+import Assignment from '../pages/Assignment.jsx'
+import DashboardComponents from '../components/DashboardComponents.jsx'
+import SideBar from '../components/Sidebar.jsx'
+import { Layout } from 'antd'
+
+const auth = false;
+
+
 const AppRouter = () => {
   return (
     <>
       <Navbar />
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/Signup' element={<Signup/>}/>
-        </Routes>
+        {auth ?
+          <>
+            <Routes>
+
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/Signup' element={<Signup />} />
+              <Route path='/assignment' element={<Assignment />} />
+            </Routes>
+
+          </>
+          :
+          <>
+            <Layout style={{ minHeight: '100vh' }} >
+              <SideBar />
+              <Routes>
+
+                <Route path='/' element={<DashboardComponents />} />
+                <Route path='/assignment' element={<Assignment />} />
+              </Routes>
+
+            </Layout>
+          </>}
       </BrowserRouter>
     </>
   )
