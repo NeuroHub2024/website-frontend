f3c8c11e837f04ff0ef1b20ac6a6a93ab3d533de
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2024-06-30 14:21:12 +0530

    user api connection done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,63 +1,132 @@
-import login_img from '../assets/login_img.png'
-import '../styles/Login.css'
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import axios from 'axios';
+// import { Link,useNavigate } from 'react-router-dom';
+// import { LockOutlined, UserOutlined } from '@ant-design/icons';
+// import { Button, Checkbox, Form, Input } from 'antd';
+// import login_img from '../assets/login_img.png';
+// import '../styles/Login.css';
+
+// function Login({onLoginSuccess}) {
+//   const navigate=useNavigate();
+//   const onFinish = async (values) => {
+//     console.log('Received values of form: ', values);
+//     const payload = {
+//       username: values.username,
+//       password: values.password,
+//     };
+//     console.log('Payload to be sent: ', payload);
+//     try {
+//       const response = await axios.post('https://gateway-mpfy.onrender.com/user/login', payload);
+//       console.log('Login successful: ', response.data);
+//       onLoginSuccess();
+//       navigate('/');
+//     } catch (error) {
+//       console.error('There was an error signing up: ', error.response ? error.response.data : error.message);
+//     }
+//   };
+
+//   return (
+//     <div className="fullpage">
+//       <div className="box">
+//         <div className="imagebox">
+//           <img className="image" src={login_img} alt="Example" />
+//         </div>
+//         <hr style={{ color: 'grey' }} />
+//         <div className="form">
+//           <Form
+//             name="normal_login"
+//             className="login-form"
+//             initialValues={{ remember: true }}
+//             onFinish={onFinish}
+//           >
+//             <Form.Item
+//               name="username"
+//               rules={[{ required: true, message: 'Please input your Username!' }]}
+//             >
+//               <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+//             </Form.Item>
+//             <Form.Item
+//               name="password"
+//               rules={[{ required: true, message: 'Please input your Password!' }]}
+//             >
+//               <Input
+//                 prefix={<LockOutlined className="site-form-item-icon" />}
+//                 type="password"
+//                 placeholder="Password"
+//               />
+//             </Form.Item>
+//             <Form.Item>
+//               <Form.Item name="remember" valuePropName="checked" noStyle>
+//                 <Checkbox>Remember me</Checkbox>
+//               </Form.Item>
+//               <a className="login-form-forgot" href="">
+//                 Forgot password
+//               </a>
+//             </Form.Item>
+//             <Form.Item>
+//               <Button type='primary' htmlType="submit" className="login-form-button">
+//                 Log in
+//               </Button>
+//             </Form.Item>
+//             Or <Link to="/Signup">register now!</Link>
+//           </Form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+import React from 'react';
+import axios from 'axios';
+import { Link,useNavigate } from 'react-router-dom';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Checkbox, Form, Input } from 'antd';
+import login_img from '../assets/login_img.png';
+import '../styles/Login.css';
 
-function Login() {
-
-  const onFinish = (values) => {
+function Login({ onLoginSuccess }) {
+  const navigate=useNavigate();
+  const onFinish = async (values) => {
     console.log('Received values of form: ', values);
+    const payload = {
+      username: values.username,
+      password: values.password,
+    };
+    console.log('Payload to be sent: ', payload);
+    try {
+      const response = await axios.post('https://gateway-mpfy.onrender.com/user/login', payload);
+      console.log('Login successful: ', response.data);
+      onLoginSuccess();
+      navigate('/');
+    } catch (error) {
+      console.error('There was an error signing up: ', error.response ? error.response.data : error.message);
+    }
   };
 
   return (
     <div className="fullpage">
       <div className="box">
         <div className="imagebox">
-          {/* <img className="image" src={login_img} alt="Example" /> */}
+          <img className="image" src={login_img} alt="Example" />
         </div>
-        <hr style={{ color: 'grey' }}></hr>
+        <hr style={{ color: 'grey' }} />
         <div className="form">
-          {/* <form id="login-form" action="/login" method="post">
-                     <p className='big_text'>Login</p>
-
-                    <div >
-                    <input  className="formgroup" type="email" id="email" name="email" placeholder='Email'required />
-                    </div>
-                    <div>
-                    <input  className="formgroup" type="password" id="password" name="password"  placeholder='Password' required />
-                    </div>
-                    <p className='sign'>New Here?<Link to="/Signup" className='signlink'>Welcome Aboard</Link></p>
-                   
-                    <button className="button" type="submit">Login</button>
-                </form> */}
           <Form
             name="normal_login"
             className="login-form"
-            initialValues={{
-              remember: true,
-            }}
+            initialValues={{ remember: true }}
             onFinish={onFinish}
           >
             <Form.Item
               name="username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Username!',
-                },
-              ]}
+              rules={[{ required: true, message: 'Please input your Username!' }]}
             >
               <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
             </Form.Item>
             <Form.Item
               name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Password!',
-                },
-              ]}
+              rules={[{ required: true, message: 'Please input your Password!' }]}
             >
               <Input
                 prefix={<LockOutlined className="site-form-item-icon" />}
@@ -69,12 +138,10 @@ function Login() {
               <Form.Item name="remember" valuePropName="checked" noStyle>
                 <Checkbox>Remember me</Checkbox>
               </Form.Item>
-
               <a className="login-form-forgot" href="">
                 Forgot password
               </a>
             </Form.Item>
-
             <Form.Item>
               <Button type='primary' htmlType="submit" className="login-form-button">
                 Log in
