2a95df0abfbae58b8229249d6bce71dbf39d75b2
ABhay Rawat <rawat141abhay@gmail.com>
2024-07-14 19:05:59 +0530

    intergation of add lecture api

--- src/containers/AnnouncementContainer.jsx
+++ src/containers/AnnouncementContainer.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AnnouncementCard from '../components/AnnouncementCard'
 import styles from '../styles/LectureContainer.module.css'
-import { Button } from 'antd'
+import { Button, Spin } from 'antd'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import Cookie from 'js-cookie'
 import { PlusCircleOutlined } from '@ant-design/icons'
 
-const AnnouncementContainer = () => {
+const AnnouncementContainer = ({ batchId, announcements }) => {
+
+  const [loading, setLoading] = useState(false);
+  const [announce, setAnnOunce] = useState([]);
+
+  const getAllAnnouncementsById = async () => {
+    setLoading(true);
+    try {
+      const token = Cookie.get('token');
+      const announcementPromises = announcements.map(announcementId =>
+        axios.get(`https://gateway-mpfy.onrender.com/announcement/${announcementId}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        })
+      );
+
+      const responses = await Promise.all(announcementPromises);
+      const announcementData = responses.map(response => response.data);
+      setAnnOunce(announcementData);
+      console.log(assignmentData);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getAllAnnouncementsById();
+  }, [announcements])
+
+
   return (
     <div>
-      <div className={styles.add_content_btn_container} ><Link to={'/add-announcement'}><Button className="add-batch-btn" type='primary' shape='round' icon={<PlusCircleOutlined />} size='middle'>Add Announcement</Button>  </Link></div>
-      <AnnouncementCard />
+      <div className={styles.add_content_btn_container} ><Link to={`/add-announcement/${batchId}`}><Button className="add-batch-btn" type='primary' shape='round' icon={<PlusCircleOutlined />} size='middle'>Add Announcement</Button>  </Link></div>
+
+      <Spin size='large' spinning={loading}>
+        {announce.map((announcement, index) => (
+          <AnnouncementCard key={index} announcement={announcement} />
+        ))}
+      </Spin>
+
     </div>
   )
 }
